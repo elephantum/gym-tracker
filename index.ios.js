e0670bce94ff6d9bcd7b21f2416f7ece28a3a96f
Andrey Tatarinov <elephantropy@gmail.com>
2016-04-17 14:37:16 +0300

    initial

--- index.ios.js
+++ index.ios.js
@@ -4,43 +4,231 @@
  */
 
 import React, {
+  Alert,
   AppRegistry,
   Component,
   StyleSheet,
+  StatusBar,
   Text,
   TextInput,
+  TouchableHighlight,
+  ListView,
   View
 } from 'react-native';
 
+import _ from 'underscore';
+
 var INITIAL_DATA = [
   {
-    id: 1,
+    name: "Эллипс",
+    reps: [
+      {
+        type: "mins",
+        n: 5
+      }
+    ]
+  },
+  {
     name: "Пресс",
-    completed: true
+    reps: [
+      { n: 20 },
+      { n: 20 },
+      { n: 20 },
+    ]
   },
   {
-    id: 2,
-    name: "Ноги",
-    completed: false
+    name: "Подъем ног",
+    reps: [
+      { n: 15 },
+      { n: 15 },
+      { n: 15 },
+    ]
   },
   {
-    id: 3,
-    name: "Дельта",
-    completed: false
+    name: "Спина-дельта",
+    reps: [
+      {
+        n: 15,
+        weight: 40
+      },
+      {
+        n: 15,
+        weight: 40
+      },
+      {
+        n: 15,
+        weight: 40
+      },
+    ]
+  },
+  {
+    name: "Спина-наклоны назад",
+    reps: [
+      {
+        n: 15,
+        weight: 50
+      },
+      {
+        n: 15,
+        weight: 50
+      },
+      {
+        n: 15,
+        weight: 50
+      },
+    ]
+  },
+  {
+    name: "Колени разгиб",
+    reps: [
+      {
+        n: 20,
+        weight: 20
+      },
+      {
+        n: 20,
+        weight: 20
+      },
+      {
+        n: 20,
+        weight: 20
+      },
+      {
+        n: 20,
+        weight: 20
+      },
+      {
+        n: 20,
+        weight: 20
+      },
+    ],
+  },
+  {
+    name: "Колени сгибания",
+    reps: [
+      {
+        n: 15,
+        weight: 50,
+      },
+      {
+        n: 15,
+        weight: 50,
+      },
+      {
+        n: 15,
+        weight: 50,
+      },
+    ]
+  },
+  {
+    name: "Грудные мышцы",
+    reps: [
+      {
+        n: 15,
+        weight: 40.5,
+      },
+      {
+        n: 15,
+        weight: 40.5,
+      },
+      {
+        n: 15,
+        weight: 40.5,
+      },
+    ]
+  },
+  {
+    name: "Трицепц",
+    reps: [
+      {
+        n: 15,
+        weight: 29,
+      },
+      {
+        n: 15,
+        weight: 29,
+      },
+      {
+        n: 15,
+        weight: 29,
+      },
+    ]
+  },
+  {
+    name: "Бицепц",
+    reps: [
+      {
+        n: 10,
+        weight: 10,
+      },
+      {
+        n: 10,
+        weight: 10,
+      },
+      {
+        n: 10,
+        weight: 10,
+      },
+    ]
+  },
+  {
+    name: "Эллипс",
+    reps: [
+      {
+        type: "mins",
+        n: 5,
+      }
+    ]
   }
 ];
 
 class GymTracker extends Component {
   constructor(props) {
     super(props);
-    this.state = {"data": INITIAL_DATA};
+    var ds = new ListView.DataSource({rowHasChanged: (a, b) => a !== b});
+    this.state = {
+      data: INITIAL_DATA,
+      currentList: ds.cloneWithRows(INITIAL_DATA)
+    };
   }
 
-  renderOneItem(item) {
+  toggleRep(rowID, repID) {
+    //Alert.alert(`${rowID}, ${repID}`, "");
+
+    t = this.state.data.slice();
+    t[rowID] = _.clone(t[rowID]);
+    t[rowID].reps[repID].complete = ! t[rowID].reps[repID].complete;
+
+    this.setState({
+      data:t,
+      currentList: this.state.currentList.cloneWithRows(t)
+    });
+  }
+
+  renderOneItem(item, sectionID, rowID, highlightRow) {
+    totalReps = item.reps.length
+    completeReps = item.reps.filter((x) => x.complete).length
+
     return (
-      <View flexDirection="row" alignItems="flex-start" key={"item-" + item.id}>
-        <Text>[{item.completed ? "x" : " "}]</Text>
-        <Text>{item.name}</Text>
+      <View flexDirection="column" style={[styles.item, totalReps == completeReps ? styles.complete : {}]}>
+        <Text style={styles.itemText}>
+          {completeReps}/{totalReps}
+          {" "}
+          {item.name}
+        </Text>
+        <View flexDirection="row" flexWrap="wrap" alignItems="flex-start">
+          {item.reps.map((rep,i) =>
+            <View key={"rep-" + i}>
+              <TouchableHighlight
+                onPress={() => this.toggleRep(rowID, i)}
+                onLongPress={() => Alert.alert("Long press")}>
+                <Text
+                  style={[styles.itemReps, rep.complete ? styles.complete : styles.incomplete]}
+                >{rep.weight ? rep.weight + "кг * " : ""}{rep.n}{rep.type == "mins" ? " мин" : ""}</Text>
+              </TouchableHighlight>
+            </View>
+          )}
+        </View>
       </View>
     );
   }
@@ -48,29 +236,42 @@ class GymTracker extends Component {
   render() {
     return (
       <View style={styles.container}>
-        {this.state.data.map((x,i) => this.renderOneItem(x))}
+        <ListView
+          dataSource={this.state.currentList}
+          renderRow={(a,b,c,d) => this.renderOneItem(a,b,c,d)}
+        />
       </View>
     );
   }
 }
 
+zoomK = 1.5
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'space-around',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: 'flex-start',
+    paddingTop: 20,
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  item: {
+    paddingTop: 10*zoomK,
+    paddingBottom: 10*zoomK,
+    paddingLeft: 10*zoomK,
+    paddingRight: 10*zoomK
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  itemText: {
+    fontSize: 18*zoomK
   },
+  itemReps: {
+    padding: 5*zoomK,
+    margin: 2*zoomK
+  },
+  complete: {
+    backgroundColor: "lightgreen"
+  },
+  incomplete: {
+    backgroundColor: "lightgray"
+  }
 });
 
 AppRegistry.registerComponent('GymTracker', () => GymTracker);
